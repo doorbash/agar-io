d29ef3f2a574964d01b9bf8cd1caa7c5ce2223ba
Milad Doorbash <milad.doorbash@gmail.com>
2020-09-20 23:47:57 +0430

    used default for initial field values

--- client/core/src/io/github/doorbash/agario/classes/Fruit.kt
+++ client/core/src/io/github/doorbash/agario/classes/Fruit.kt
@@ -3,20 +3,21 @@ package io.github.doorbash.agario.classes
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.Vector2
 import io.colyseus.annotations.SchemaField
+import io.colyseus.default
 import io.colyseus.serializer.schema.Schema
 
 class Fruit : Schema() {
     @SchemaField("0/string")
-    var key:String? = null
+    var key:String? = String.default
 
     @SchemaField("1/float32")
-    var x = 0f
+    var x = Float.default
 
     @SchemaField("2/float32")
-    var y = 0f
+    var y = Float.default
 
     @SchemaField("3/int32")
-    var color = 0
+    var color = Int.default
 
     var position = Vector2()
     var _color: Color? = null
--- client/core/src/io/github/doorbash/agario/classes/Player.kt
+++ client/core/src/io/github/doorbash/agario/classes/Player.kt
@@ -3,20 +3,21 @@ package io.github.doorbash.agario.classes
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.math.Vector2
 import io.colyseus.annotations.SchemaField
+import io.colyseus.default
 import io.colyseus.serializer.schema.Schema
 
 class Player : Schema() {
     @SchemaField("0/float32")
-    var x = 0f
+    var x = Float.default
 
     @SchemaField("1/float32")
-    var y = 0f
+    var y = Float.default
 
     @SchemaField("2/float32")
-    var radius = 0f
+    var radius = Float.default
 
     @SchemaField("3/int32")
-    var color = 0
+    var color = Int.default
 
     var position = Vector2()
     var _color: Color? = null
